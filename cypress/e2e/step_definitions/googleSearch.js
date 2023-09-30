7f32858c1cddc76e13a799a58c70a32db8cd05da
Ezequiel Gonzalez <ezequielgz88@gmail.com>
2023-09-30 18:44:45 +0200

    add POM and reporting configurations

--- cypress/e2e/step_definitions/googleSearch.js
+++ cypress/e2e/step_definitions/googleSearch.js
@@ -1,27 +1,26 @@
 import {
     Given,
     When,
-    And,
     Then
 } from "@badeball/cypress-cucumber-preprocessor";
+import GoogleSearchPage from "../../pages/GoogleSearchPage";
 
 Given('the user opens the Google homepage', () => {
-    cy.visit("https://www.google.com")
-    cy.acceptCookies()
+    GoogleSearchPage.visit();
 });
 
-When("the user searches for {string}", (searchTerm) => {
-    cy.get("#APjFqb").type(searchTerm).type("{enter}");
+When("the user searches for {string}", (criteria) => {
+    GoogleSearchPage.searchFor(criteria);
 });
 
-When("the user clicks on the {string} link in the search results", (searchTerm) => {
-    cy.get("span.VuuXrf").contains(searchTerm).click();
+When("the user clicks on the {string} link in the search results", (pageLink) => {
+    GoogleSearchPage.clickSearchResult(pageLink);
 });
 
-Then("the user should be on the {string} page", (searchTerm) => {
-    cy.url().should("include", searchTerm);
+Then("the user should be on the {string} page", (web) => {
+    GoogleSearchPage.assertOnPage(web);
 });
 
 Then("the user should find the text {string} on the page", (validateText) => {
-    cy.get("body").contains(validateText).screenshot({ capture: 'viewport', overwrite: true });
+    GoogleSearchPage.assertTextOnPage(validateText);
 });
